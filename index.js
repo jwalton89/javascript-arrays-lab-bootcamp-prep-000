a8558962be63e340c86da567b4f3e715a6ced49b
Jeff Walton <jwalton89@gmail.com>
2017-11-28 00:51:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 var name = "Taz Kitty"
-var array = kittens 
+var array = kittens
 
 function destructivelyAppendKitten(array, name) {
-  return name.push(name)
+  return array.push(name)
 }
